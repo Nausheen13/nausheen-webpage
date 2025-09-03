eb2771f887a92922d0481d2c2d5939e778bdbb72
Sahid Ahmed <ahmedsahidsa786@gmail.com>
2025-09-03 15:24:48 +0530

    fix issue

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   
-  base: '/nausheen-webpage', // Relative base path
+  base: '/nausheen-webpage/', // Relative base path
   server: {
     host: "::",
     port: 8080,
